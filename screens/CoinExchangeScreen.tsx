8ae6b98caa8d11e739e93b4fa9c8cbfd613cd505
Damain Ramsajan <dpramsajan@gmail.com>
2021-02-24 08:55:56 +0100

    Coin exchange logic complete

--- screens/CoinExchangeScreen.tsx
+++ screens/CoinExchangeScreen.tsx
@@ -1,10 +1,13 @@
 import { useRoute } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Image, Dimensions, TextInput, } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions, TextInput, Pressable, Alert, KeyboardAvoidingView, } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const CoinExchangeScreen = () => {
     const [coinAmount, setCoinAmount] = useState ("");
     const [coinUSDValue, setCoinUSDValue] = useState ("");
+
+    const maxUSD = 100000;
 
     const route = useRoute ();
 
@@ -13,16 +16,39 @@ const CoinExchangeScreen = () => {
     
     useEffect(() => {
         const amount = parseFloat(coinAmount)
-        if (!amount) {
-            setCoinAmount("0");
+        if (!amount && amount !== 0) {
+            setCoinAmount("");
+            setCoinUSDValue("");
             return;
         }
-       setCoinUSDValue((coinData?.currentPrice * amount).toString())
+        setCoinAmount(amount.toString())
+        setCoinUSDValue((coinData?.currentPrice * amount).toString())
         
-    }, [coinAmount])
+    }, [coinAmount]);
+
+    useEffect(() => {
+        const amount = parseFloat(coinUSDValue)
+        if (!amount && amount !== 0) {
+            setCoinAmount("");
+            setCoinUSDValue("");
+            return;
+        }
+        setCoinUSDValue(amount.toString())
+        setCoinAmount((amount/coinData?.currentPrice ).toString())
+        
+    }, [coinUSDValue]);
+
+    const placeOrder = () => {
+        if (isBuy && parseFloat(coinUSDValue) > maxUSD){
+            Alert.alert(`Not enough USD Money. Max = ${maxUSD}`)
+        }
+        if (!isBuy && coinAmount > coinData.amount){
+            Alert.alert(`Not enough ${coinData.symbol} Coins. Max = ${coinData.amount}`)
+        }
+    }
 
     return (
-        <View>
+    <KeyboardAvoidingView behavior = {"padding"} style = {{flex: 1}} keyboardVerticalOffset = {100}>
             <Image style = {styles.image} source ={require("../assets/images/exchange.jpg")} />
             <Text style={styles.heading}>{isBuy? "Buy " : "Sell "} {coinData.name}</Text>
             <Text style = {styles.description}>1 {coinData.symbol} = ${coinData.currentPrice} USD</Text>    
@@ -36,6 +62,15 @@ const CoinExchangeScreen = () => {
                         onChangeText = {setCoinAmount}
                     />
                     <Text>{coinData.symbol}</Text>
+                    {/* <View style = {styles.caretContainer}>
+                        <Pressable >
+                            <Ionicons name="ios-caret-up" size={18} color="black" />
+                        </Pressable>
+
+                        <Pressable>
+                            <Ionicons name="ios-caret-down" size={18} color="black" />
+                        </Pressable>    
+                    </View> */}
                 </View>
 
                 <Text>=</Text>
@@ -48,9 +83,16 @@ const CoinExchangeScreen = () => {
                         onChangeText = {setCoinUSDValue}
                     />
                     <Text>USD</Text>
-                </View>
+                </View>                               
             </View>
-        </View>
+            <Text style = {{alignSelf: "flex-end", marginRight: 76}}>Max: ${maxUSD} </Text> 
+            <View >
+                <Pressable style = {styles.button} onPress={placeOrder}>
+                    <Text style = {styles.buttonText}>Place Order</Text>
+                </Pressable>
+            </View>
+            <View style = {{flex: 1}}/>
+        </KeyboardAvoidingView>
     )
 }
 
@@ -58,10 +100,10 @@ export default CoinExchangeScreen
 
 const styles = StyleSheet.create({
     image: {
-        marginTop: -12,
-        height: Dimensions.get("screen").height/3, 
+        marginTop: -14,
+        height: 280, 
         width: Dimensions.get("screen").width, 
-        resizeMode: "contain",
+        resizeMode: "cover",
     },
     heading:{
         alignSelf: "center",
@@ -73,7 +115,7 @@ const styles = StyleSheet.create({
         fontSize: 14,
         // fontWeight: "bold",
         textAlign: "center",
-        marginBottom: 10
+        // marginBottom: 10
     },
     inputGroup: {
         flexDirection: "row", 
@@ -81,7 +123,7 @@ const styles = StyleSheet.create({
         width: Dimensions.get("screen").width-20, 
         alignItems: "center",
         marginHorizontal: 10,
-        marginTop: 20,
+        marginTop: 10,
     },
     inputContainer: {
         flexDirection: "row",
@@ -89,6 +131,25 @@ const styles = StyleSheet.create({
         borderColor: "lightgrey",
         alignItems: "center",
         width: 160,
+        height: 45,
         padding: 10,
     },
+    button: {        
+        width: 360,
+        height: 48,
+        borderRadius: 40,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#5AB858",
+        marginTop: 10,
+        marginBottom: 20,
+        marginHorizontal: 20,
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 24,
+    },
+    caretContainer: {
+        marginLeft: 70
+    }
 })
